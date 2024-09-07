2cdbc1ef883da45be704e76059633e9b29cc1f18
배석현 <bjh4966@naver.com>
2024-09-07 15:38:31 +0900

    11

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") //order의 Many to one 이랑 연결  order table에 있는 Member member..맵핑된 거울. 읽기전용
+    private List<Order> orders = new ArrayList<Order>();
+}
