e2d1806f26a38112ef700498823633b86f67fc4e
배석현 <bjh4966@naver.com>
2024-09-08 19:51:43 +0900

    9. 조회용 Sample Data

--- jpashop/src/main/java/jpabook/jpashop/InitDb.java
+++ jpashop/src/main/java/jpabook/jpashop/InitDb.java
@@ -12,16 +12,20 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class InitDb {
     private final InitService initService;
+
     @PostConstruct
     public void init() {
         initService.dbInit1();
         initService.dbInit2();
     }
+
     @Component
     @Transactional
     @RequiredArgsConstructor
     static class InitService {
+
         private final EntityManager em;
+
         public void dbInit1() {
             Member member = createMember("userA", "서울", "1", "1111");
             em.persist(member);
@@ -31,10 +35,10 @@ public class InitDb {
             em.persist(book2);
             OrderItem orderItem1 = OrderItem.createOrderItem(book1, 10000, 1);
             OrderItem orderItem2 = OrderItem.createOrderItem(book2, 20000, 2);
-            Order order = Order.createOrder(member, createDelivery(member),
-                    orderItem1, orderItem2);
+            Order order = Order.createOrder(member, createDelivery(member), orderItem1, orderItem2);
             em.persist(order);
         }
+
         public void dbInit2() {
             Member member = createMember("userB", "진주", "2", "2222");
             em.persist(member);
@@ -45,17 +49,17 @@ public class InitDb {
             Delivery delivery = createDelivery(member);
             OrderItem orderItem1 = OrderItem.createOrderItem(book1, 20000, 3);
             OrderItem orderItem2 = OrderItem.createOrderItem(book2, 40000, 4);
-            Order order = Order.createOrder(member, delivery, orderItem1,
-                    orderItem2);
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
             em.persist(order);
         }
-        private Member createMember(String name, String city, String street,
-                                    String zipcode) {
+
+        private Member createMember(String name, String city, String street, String zipcode) {
             Member member = new Member();
             member.setName(name);
             member.setAddress(new Address(city, street, zipcode));
             return member;
         }
+
         private Book createBook(String name, int price, int stockQuantity) {
             Book book = new Book();
             book.setName(name);
@@ -63,6 +67,7 @@ public class InitDb {
             book.setStockQuantity(stockQuantity);
             return book;
         }
+
         private Delivery createDelivery(Member member) {
             Delivery delivery = new Delivery();
             delivery.setAddress(member.getAddress());
