a819bc37df5f8e45c78490c0c942fbbe92fd80f3
배석현 <bjh4966@naver.com>
2024-09-09 20:23:04 +0900

    20. 주문 조회 V6: JPA에서 DTO로 직접 조회, 플랫 데이터 최적화

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -2,6 +2,8 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.*;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.order.query.OrderFlatDto;
+import jpabook.jpashop.repository.order.query.OrderItemQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Data;
@@ -13,7 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
-import static java.util.stream.Collectors.toList;
+
+import static java.util.stream.Collectors.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -70,7 +73,21 @@ public class OrderApiController {
         return orderQueryRepository.findAllByDto_optimization();
     }
 
-    @Data
+    @GetMapping("/api/v6/orders")
+    public List<OrderQueryDto> ordersV6() {
+        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+        return flats.stream()
+                .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(),o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+                        mapping(o -> new OrderItemQueryDto(o.getOrderId(),
+                                o.getItemName(), o.getOrderPrice(), o.getCount()), toList())
+                )).entrySet().stream()
+                .map(e -> new OrderQueryDto(e.getKey().getOrderId(),
+                        e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(),
+                        e.getKey().getAddress(), e.getValue()))
+                .collect(toList());
+    }
+
+        @Data
     // or @Getter 둘중 하나.
     static class OrderDto{
         private Long orderId;
--- jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderItemQueryDto.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderItemQueryDto.java
@@ -5,7 +5,6 @@ import lombok.Data;
 
 @Data
 public class OrderItemQueryDto {
-
     @JsonIgnore
     private Long orderId; //주문번호
     private String itemName;//상품 명
